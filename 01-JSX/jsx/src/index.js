b9906e856f78336cd5fb4d5bd9bd7ccc51604e64
Na Lin <lna483018@gmail.com>
2022-10-27 16:53:04 -0400

    5 steps to create and show components on browser

---
+++ 01-JSX/jsx/src/index.js
@@ -0,0 +1,17 @@
+// todo 1: import the React and ReactDOM libraries
+import React from "react"; // understand what component is and make multiple components work together
+import ReactDOM from "react-dom/client"; // know how to get a component turn into HTML to show up in the browser
+
+// todo 2: get a reference to the div with ID root
+const el = document.getElementById("root"); //place render into div
+
+// todo 3: tell react to take control of that element
+const root = ReactDOM.createRoot(el); // tell react show application inside this el
+
+// todo 4: create a component, a function that return jsx
+function App() {
+  return <h1>Hello World!</h1>;
+}
+
+// todo 5: show the component on the screen
+root.render(<App />); // input a App as a jsx element
