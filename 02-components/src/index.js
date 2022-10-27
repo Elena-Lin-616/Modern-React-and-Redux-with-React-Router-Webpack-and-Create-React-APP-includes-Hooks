2983ff0d1a8ad95490750d8c6d14416b4e6e7a3f
Na Lin <lna483018@gmail.com>
2022-10-27 17:53:47 -0400

    second way to create and show components

---
+++ 02-components/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <h2>hello world</h2>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
